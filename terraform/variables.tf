df49f3a6fc2fdede77d646153ed64f15fed552ec
Maria <marysiula.bielawa@gmail.com>
2026-01-17 13:45:32 +0100

    terraform updates

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,7 @@
+variable "resource_group" {}
+variable "location" { default= "Sweden Central" }
+variable "storage_account_name" {}
+variable "eventhub_namespace_name" {}
+variable "eventhub_name" {}
+variable "databricks_workspace_name" {}
 
